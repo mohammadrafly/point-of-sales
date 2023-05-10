ff2d0299687217fe5076265a05d42548e2c181d6
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-05-10 15:53:51 +0700

    update path for vendors sb admin 2

--- app/Views/layout/templateDashboard.php
+++ app/Views/layout/templateDashboard.php
@@ -9,12 +9,12 @@
 
     <title>Penjualan App</title>
 
-    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/vendors/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendors/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
 </head>
 <body id="page-top">
     <div id="wrapper">
@@ -116,12 +116,12 @@
     </div>
     <script src="<?= base_url('assets/js/Main.js') ?>"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-    <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendors/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendors/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendors/jquery-easing/jquery.easing.min.js') ?>"></script>
     <script src="<?= base_url('assets/js/sb-admin-2.min.js') ?>"></script>
-    <script src="<?= base_url('assets/vendor/datatables/jquery.dataTables.min.js') ?>"></script>
-    <script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendors/datatables/jquery.dataTables.min.js') ?>"></script>
+    <script src="<?= base_url('assets/vendors/datatables/dataTables.bootstrap4.min.js') ?>"></script>
     <script src="<?= base_url('assets/js/demo/datatables-demo.js') ?>"></script>
     <?= $this->renderSection('script') ?>
 </body>
